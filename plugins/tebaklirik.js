a7ab1e3edfa2f5502882846ae0b979df607a3c49
Saipul Anuar <drakipul1016@gmail.com>
2021-12-25 11:15:26 +0700

    wa bot

---
+++ plugins/tebaklirik.js
@@ -0,0 +1,35 @@
+let fetch = require('node-fetch')
+let timeout = 120000
+let poin = 500
+let handler = async (m, { conn, usedPrefix }) => {
+    conn.tebaklirik = conn.tebaklirik ? conn.tebaklirik : {}
+    let id = m.chat
+    if (id in conn.tebaklirik) {
+        conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.tebaklirik[id][0])
+        throw false
+    }
+    let res = await fetch(API('amel', '/tebaklirik', {}, 'apikey'))
+    if (!res.ok) throw eror
+    let json = await res.json()
+    if (!json.status) throw json
+    let caption = `
+${json.soal}
+
+Timeout *${(timeout / 1000).toFixed(2)} detik*
+Ketik ${usedPrefix}teli untuk bantuan
+Bonus: ${poin} XP
+`.trim()
+    conn.tebaklirik[id] = [
+        await conn.sendButton(m.chat, caption, '© stikerin', 'Bantuan', `.teli`, m),
+        json, poin,
+        setTimeout(async () => {
+            if (conn.tebaklirik[id]) await conn.sendButton(m.chat, `Waktu habis!\nJawabannya adalah *${json.jawaban}*`, '© stikerin', 'Tebak Lirik', `.tebaklirik`, conn.tebaklirik[id][0])
+            delete conn.tebaklirik[id]
+        }, timeout)
+    ]
+}
+handler.help = ['tebaklirik']
+handler.tags = ['game']
+handler.command = /^tebaklirik/i
+
+module.exports = handler
